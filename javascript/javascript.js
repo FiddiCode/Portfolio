0eeb3233e3d8b0852dd3f15e9b56218e255d0e05
fiddicode <coderfardeen@gmail.com>
2023-09-01 10:31:05 +0530

    new file:   images/fardeen.jpg 	new file:   images/fardeen.png 	modified:   index.html 	modified:   javascript/javascript.js 	modified:   style/style.css

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,7 +2,10 @@
 let menuIcon=document.querySelector("#menu-icon");
 let navbar=document.querySelector(".navbar");
 
-
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 
 // ===================scroll section active navbar==================
@@ -35,5 +38,23 @@ window.onscroll=()=>{
 
     let header= document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // / ====================toggle icon navbar===================
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
 
+
+// ====================scroll reveal====================
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-image', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
